03f41ee0cf41357acb770d873c1c5b4c8ffd3a9e
Emma Dickson <ejd315@nyu.edu>
2016-11-25 15:44:53 -0500

    syllable count working

--- analysis.py
+++ analysis.py
@@ -6,39 +6,61 @@ from curses.ascii import isdigit
 
 
 d = cmudict.dict()
-
+personal_OOV = {}
 
 def syllable_count(word):
     s = " "
 
     if word in d:
         lists = d[word]
-        pronounciation = s.join(lists[0])
-        print word + " and its pronounciation " + pronounciation
-        print len(re.findall('\d+', pronounciation))
+        pronunciation = s.join(lists[0])
+        # print word + " and its pronunciation " + pronunciation
+        # print len(re.findall('\d+', pronunciation))
+        return len(re.findall('\d+', pronunciation))
 
-    print "OOV word encountered"
-    subtract_syllables = ["cial", "tia", "cius", "cious", "uiet", "gious", "priest", "giu", "dge", "e$", "des$",
-                          "mes$", "kes$", "nce$", "rles$"]
-    add_syllables = ["ia", "riet", "dien", "ien", "iet", "iu", "iest", "io", "ii", "ily", "oala$", "iara$", "ying$",
-                     "eate$", "eation$"]
-    subtract_total = 0
-    addition_total = 0
+    else:
+        if word in personal_OOV:
+            return personal_OOV[word]
+        else:
 
-    for subtract in subtract_syllables:
-        if re.search(subtract, word) != None:
-            subtract_total += 1
-    for add in add_syllables:
-        if re.search(add, word) != None:
-            addition_total += 1
+            # print "OOV word encountered"
+            # The reference from which we determine if we subtract or add syllables
+            subtract_syllables = ["cial", "tia", "cius", "cious", "uiet", "gious", "priest", "giu", "dge", "e$", "des$",
+                                  "mes$", "kes$", "nce$", "rles$"]
 
-    print "original word " + word
+            add_syllables = ["ia", "riet", "dien", "ien", "iet", "iu", "iest", "io", "ii", "ily", "oala$", "iara$", "ying$",
+                             "eate$", "eation$"]
 
-    word = re.compile("[^aeiouy]").split(word)
-    for w in word:
-        if w == '':
-            word.remove(w)
-    syllable_count = len(word) - subtract_total + addition_total
-    print syllable_count
+            # original totals
+            subtract_total = 0
+            addition_total = 0
+
+            # Check using regex if we need to add or subtract syllables after determining vowel count
+
+            for subtract in subtract_syllables:
+                if re.search(subtract, word) is not None:
+                    subtract_total += 1
+
+            for add in add_syllables:
+                if re.search(add, word) is not None:
+                    addition_total += 1
+
+            # print "original word " + word
+
+            # Count the number of vowels in the word, this will be our original syllable count
+            word = re.compile("[^aeiouy]").split(word)
+            for w in word:
+                if w == '':
+                    word.remove(w)
+
+            # Calculate the final syllable count
+            syllable_total = len(word) - subtract_total + addition_total
+
+            # Add total to OOV dict
+            personal_OOV[word] = syllable_total
+
+            # Return total syllable count
+            return syllable_total
+
 
 syllable_count("caterpillar")
